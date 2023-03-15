91b6a80a853c3444998f9f4eaa6f2d684182ad5c
varma20 <105639509+varma20@users.noreply.github.com>
2023-03-15 21:07:20 +0530

    Add files via upload

---
+++ JDBC/Select.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+public class Select{
+	public static void main(String[] args) throws Exception{
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/adv_java";
+		String user = "root";
+		String pwd = "Varma";
+		
+		Class.forName(driver);
+		Connection con = DriverManager.getConnection(url,user,pwd);
+		Statement st = con.createStatement();
+		
+		String sql = "Select * from employee1";
+		boolean flag = false;
+		ResultSet rs = st.executeQuery(sql);
+		System.out.println("eid \t ename \t esal\t");
+		System.out.println("-----------------------------------------------------");
+		while(rs.next()){
+			flag = true;
+			System.out.println(rs.getInt(1) + "\t" + rs.getString(2) + "\t" + rs.getInt(3) + "\t");
+		}
+		if(flag == false){
+			System.out.println("No Records Found");
+		}
+	}
+}
